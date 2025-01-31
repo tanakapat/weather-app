23264b9881dd031a9f83608093de360e36d528d4
tanakapat <patricia.tanaka.3@hotmail.com>
2025-01-31 11:51:00 -0300

    Added api key and url

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+function searchCity(city) {
+    let apiKey = "bo5101a34b5992391t9e44f085355a8b";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    console.log(apiUrl)
+};
+
 function handleSearchSubmit(event) {
     event.preventDefault();
 
     let searchInput = document.querySelector("#search-form-input");
     let currentCityElement = document.querySelector("#new-city");
     currentCityElement.innerHTML = searchInput.value;
-}
+    searchCity(searchInput.value);
+};
 
 let searchFormElement = document.querySelector("#city-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit)
