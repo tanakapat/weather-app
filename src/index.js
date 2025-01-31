334a0d36ad8f12d8ef67138a3538dd60036db11c
tanakapat <patricia.tanaka.3@hotmail.com>
2025-01-31 13:22:59 -0300

    Added city name and temperature changes using Shecodes API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
+function updateWeather(response) {
+    let temperatureElement = document.querySelector("#current-temperature");
+    let temperature = response.data.temperature.current;
+
+   let currentCityElement = document.querySelector("#new-city");
+
+    currentCityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+};
+
 function searchCity(city) {
     let apiKey = "bo5101a34b5992391t9e44f085355a8b";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-    console.log(apiUrl)
+    axios.get(apiUrl).then(updateWeather);
 };
 
 function handleSearchSubmit(event) {
     event.preventDefault();
 
     let searchInput = document.querySelector("#search-form-input");
-    let currentCityElement = document.querySelector("#new-city");
-    currentCityElement.innerHTML = searchInput.value;
+    
     searchCity(searchInput.value);
 };
 
