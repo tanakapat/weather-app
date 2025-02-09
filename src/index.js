09ea20a2dfe8a04b9bf585e74d7b24172c74e36a
tanakapat <patricia.tanaka.3@hotmail.com>
2025-02-09 08:36:25 -0300

    Added date and time updates

--- src/index.js
+++ src/index.js
@@ -8,13 +8,18 @@ function updateWeather(response) {
 
     let humidityElement = document.querySelector("#humidity");
 
-    let windSpeedElement = document.querySelector("#wind-speed")
+    let windSpeedElement = document.querySelector("#wind-speed");
+
+    let timeElement = document.querySelector("#time");
+
+    let date = new Date(response.data.time * 1000);
    
     currentCityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
     weatherDescriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
+    timeElement.innerHTML = formatDate(date);
 };
 
 function searchCity(city) {
@@ -30,6 +35,21 @@ function handleSearchSubmit(event) {
     
     searchCity(searchInput.value);
 };
+
+function formatDate(date) {    
+    let minutes = date.getMinutes();
+
+    let hours = date.getHours();
+    
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+    }
+
+    return `${day} ${hours}:${minutes}`
+}
 
 let searchFormElement = document.querySelector("#city-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit)
